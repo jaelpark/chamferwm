82f40a9d0845ea6703607bccb1ce5a3a3200f1cd
jparkkil <jasper.parkkila@cern.ch>
2018-11-05 01:44:52 +0100

    Improved window decorations

--- shaders/frame.hlsl
+++ shaders/frame.hlsl
@@ -34,7 +34,7 @@ const float2 vertices[4] = {
 	xy1
 };
 
-[maxvertexcount(8)]
+[maxvertexcount(12)]
 //void main(point GS_INTPUT input[1], inout TriangleStream<GS_OUTPUT> stream){
 void main(point float2 posh[1], inout TriangleStream<GS_OUTPUT> stream){
 	GS_OUTPUT output;
@@ -42,25 +42,38 @@ void main(point float2 posh[1], inout TriangleStream<GS_OUTPUT> stream){
 	float2 aspect = float2(1.0f,screen.x/screen.y);
 	float2 borderWidth = border*aspect; //this results in borders half the gap size
 
-	borderWidth *= 2.0f;
+	borderWidth *= 2.0f; //stretch to double to allow room for the effects
 
+	//window shadow
 	[unroll]
 	for(uint i = 0; i < 4; ++i){
-		output.posh = float4(vertices[i]+(2.0*vertexPositions[i]-1.0f)*borderWidth,0,1);
+		output.posh = float4(vertices[i]+(2.0*vertexPositions[i]-1.0f)*4.0f*borderWidth,0,1);
 		output.texc = vertexPositions[i];
 		output.geomId = 0;
 		stream.Append(output);
 	}
 	stream.RestartStrip();
 
+	//window border
 	[unroll]
 	for(uint i = 0; i < 4; ++i){
-		output.posh = float4(vertices[i],0,1);
-		output.texc = vertexPositions[i]; //TODO: should be adjusted according to dimensions of the box. Push constants need the maximum known border width
+		output.posh = float4(vertices[i]+(2.0*vertexPositions[i]-1.0f)*borderWidth,0,1);
+		output.texc = vertexPositions[i];
 		output.geomId = 1;
 		stream.Append(output);
 	}
 	stream.RestartStrip();
+
+	//window contents
+	[unroll]
+	for(uint i = 0; i < 4; ++i){
+		output.posh = float4(vertices[i],0,1);
+		output.texc = vertexPositions[i];
+		output.geomId = 2;
+		stream.Append(output);
+	}
+	stream.RestartStrip();
+
 }
 
 #elif defined(SHADER_STAGE_PS)
@@ -84,26 +97,39 @@ float4 main(float4 posh : SV_Position, float2 texc : TEXCOORD0, uint geomId : ID
 	float2 p1 = screen*(0.25f*(xy0+xy1)+0.5f);
 	float2 m = screen*(0.5f*xy1+0.5f);
 	float2 d1 = m-p; //d1: extent of the window
-	if(length(max(abs(posh.xy-p1)-(0.5f*d1-50.0f),0.0f))-75.0f > 0.0f)
-		discard;
+	float2 constScaling = screen.x/(d1.x+2.0f*screen.x*borderWidth.x);
+
 	if(geomId == 0){
-		//if(length(max(abs(posh.xy-p1)-(0.5f*d1-180.0f),0.0f))-100.0f < 0.0f)
-			//discard;
+		float2 q = abs(posh.xy-p1);
+		if(length(max(q-(0.5f*d1-40.0f),0.0f))-40.0f < 0.0f)
+			discard; //remove background to allow for transparency effects
+		float d = (length(max(abs((posh.xy-p1)/(1.0f+0.015f*constScaling.x))-(0.5f*d1-50.0f),0.0f))-75.0f)*1.015f;//-min(max(q.x,q.y),0.0f)*(1.0f+0.015f*constScaling.x);
+
+		return float4(0.0f,0.0f,0.0f,0.9f*saturate(-d/30.0f));
+
+	}else
+	if(geomId == 1){
+		if(length(max(abs(posh.xy-p1)-(0.5f*d1-50.0f),0.0f))-75.0f > 0.0f)
+			discard;
+		if(length(max(abs(posh.xy-p1)-(0.5f*d1-40.0f-2.0f),0.0f))-40.0f < 0.0f)
+			discard; //remove background to allow for transparency effects
 		c = float4(0,0,0,1);
 		if(flags & FLAGS_FOCUS)
 			//dashed line around focus
-			if(any(posh > p1-0.5f*d1 && posh < p1+0.5f*d1 && fmod(floor(posh/50.0f),3.0f) < 0.5f) &&
-				any(posh < p1-0.5f*d1-0.25f*screen*borderWidth || posh > p1+0.5f*d1+0.25f*screen*borderWidth))
+			if((any(posh > p1-0.5f*d1 && posh < p1+0.5f*d1 && fmod(floor(posh/50.0f),3.0f) < 0.5f) &&
+				any(posh < p1-0.5f*d1-0.25f*screen*borderWidth || posh > p1+0.5f*d1+0.25f*screen*borderWidth)))// ||
 			//edge highlight
-			//if(all(posh > p1+0.4f*d1 || posh < p1-0.4f*d1) &&
-			//	any(posh < p1-0.5f*d1-0.25f*screen*borderWidth || posh > p1+0.5f*d1+0.25f*screen*borderWidth))
+			//(all(posh > p1+0.4f*d1 || posh < p1-0.4f*d1)))
 				c.xyz = float3(1.0f,0.6f,0.33f);
+
 	}else{
 		if(length(max(abs(posh.xy-p1)-(0.5f*d1-40.0f),0.0f))-40.0f > 0.0f)
 			discard;
-		//remove transparency around the edges
-		//if(length(max(abs(posh.xy-p1)-(0.5f*d1-180.0f),0.0f))-100.0f > 0.0f)
-		//	c.w = 1.0f;
+	
+		float2 period = float2(1.8f*0.5f*d1.x-70.0f*constScaling.x,400.0f);
+		float2 q = fmod(posh.xy-p1,period)-0.5f*period;
+		if(length(max(abs(q)-float2(10.0f/(0.5f*d1.x),100.0f),0.0f))-40.0f > 0.0f)
+			c.w = 1.0f;
 	}
 
 	return c;
