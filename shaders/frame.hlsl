1babf6de75a55b863c43858db1d6f499e7c2c5a8
jparkkil <jasper.parkkila@cern.ch>
2018-12-01 16:20:06 +0200

    Shader improvements

--- shaders/frame.hlsl
+++ shaders/frame.hlsl
@@ -88,48 +88,55 @@ float4 main(float4 posh : SV_Position, float2 texc : TEXCOORD0, uint geomId : ID
 	float2 aspect = float2(1.0f,screen.x/screen.y);
 	float2 borderWidth = border*aspect; //this results in borders half the gap size
 
-	//float4 c = content.Sample(sm,texc);
 	float2 p = screen*(0.5f*xy0+0.5f);
-	float2 r = posh.xy-p;
-	float4 c = content.Load(float3(r,0)); //p already has the 0.5f offset
-	//^^returns black when out of bounds (border)
 
 	float2 p1 = screen*(0.25f*(xy0+xy1)+0.5f);
 	float2 m = screen*(0.5f*xy1+0.5f);
 	float2 d1 = m-p; //d1: extent of the window
 	float2 constScaling = screen.x/(d1.x+2.0f*screen.x*borderWidth.x);
 
+	float4 c = float4(0.0f,0.0f,0.0f,1.0f);
 	if(geomId == 0){
 		float2 q = abs(posh.xy-p1);
-		if(length(max(q-(0.5f*d1-40.0f),0.0f))-40.0f < 0.0f)
+		if(length(max(q-(0.5f*d1-40.0f),0.0f))-40.0f < 0.0f){
 			discard; //remove background to allow for transparency effects
+			return c;
+		}
 		float d = (length(max(abs((posh.xy-p1)/(1.0f+0.015f*constScaling.x))-(0.5f*d1-50.0f),0.0f))-75.0f)*1.015f;//-min(max(q.x,q.y),0.0f)*(1.0f+0.015f*constScaling.x);
 
 		return float4(0.0f,0.0f,0.0f,0.9f*saturate(-d/30.0f));
 
 	}else
 	if(geomId == 1){
-		if(length(max(abs(posh.xy-p1)-(0.5f*d1-50.0f),0.0f))-75.0f > 0.0f)
+		if(length(max(abs(posh.xy-p1)-(0.5f*d1-50.0f),0.0f))-75.0f > 0.0f){
 			discard;
-		if(length(max(abs(posh.xy-p1)-(0.5f*d1-40.0f-2.0f),0.0f))-40.0f < 0.0f)
+			return c;
+		}
+		if(length(max(abs(posh.xy-p1)-(0.5f*d1-40.0f-2.0f),0.0f))-40.0f < 0.0f){
 			discard; //remove background to allow for transparency effects
-		c = float4(0,0,0,1);
+			return c;
+		}
 		if(flags & FLAGS_FOCUS)
 			//dashed line around focus
 			if((any(posh > p1-0.5f*d1 && posh < p1+0.5f*d1 && fmod(floor(posh/50.0f),3.0f) < 0.5f) &&
-				any(posh < p1-0.5f*d1-0.25f*screen*borderWidth || posh > p1+0.5f*d1+0.25f*screen*borderWidth)))// ||
-			//edge highlight
-			//(all(posh > p1+0.4f*d1 || posh < p1-0.4f*d1)))
+				any(posh < p1-0.5f*d1-0.25f*screen*borderWidth || posh > p1+0.5f*d1+0.25f*screen*borderWidth)))
 				c.xyz = float3(1.0f,0.6f,0.33f);
 
 	}else{
-		if(length(max(abs(posh.xy-p1)-(0.5f*d1-40.0f),0.0f))-40.0f > 0.0f)
+		if(length(max(abs(posh.xy-p1)-(0.5f*d1-40.0f),0.0f))-40.0f > 0.0f){
+			return c;
 			discard;
+		}
 	
-		float2 period = float2(1.8f*0.5f*d1.x-70.0f*constScaling.x,400.0f);
-		float2 q = fmod(posh.xy-p1,period)-0.5f*period;
-		if(length(max(abs(q)-float2(10.0f/(0.5f*d1.x),100.0f),0.0f))-40.0f > 0.0f)
-			c.w = 1.0f;
+		//float4 c = content.Sample(sm,texc);
+		float2 r = posh.xy-p;
+		c = content.Load(float3(r,0)); //p already has the 0.5f offset
+		//^^returns black when out of bounds (border)
+
+		//float2 period = float2(1.8f*0.5f*d1.x-70.0f*constScaling.x,400.0f);
+		//float2 q = fmod(posh.xy-p1,period)-0.5f*period;
+		//if(length(max(abs(q)-float2(10.0f/(0.5f*d1.x),100.0f),0.0f))-40.0f > 0.0f)
+			//c.w = 1.0f;
 	}
 
 	return c;
