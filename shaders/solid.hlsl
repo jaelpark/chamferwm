1a22f0f4ff851629de34ccc1dfae30625b3034b5
jparkkil <jasper.parkkila@cern.ch>
2019-08-18 14:08:29 +0200

    Add texc

--- shaders/solid.hlsl
+++ shaders/solid.hlsl
@@ -1,7 +1,7 @@
 
 #if defined(SHADER_STAGE_PS)
 
-float4 main(float4 posh : SV_Position) : SV_Target{
+float4 main(float4 posh : SV_Position, float2 texc : TEXCOORD0) : SV_Target{
 	return float4(1,1,1,1);
 }
 
