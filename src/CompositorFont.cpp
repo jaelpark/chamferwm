f725c88eb0a4b2cc4af13791c9fd8217191d3933
jparkkil <jasper.parkkila@cern.ch>
2019-08-21 23:52:10 +0300

    Add font rendering sources and freetype2 dependency

---
+++ src/CompositorFont.cpp
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "container.h"
+#include "backend.h"
+#include "CompositorFont.h"
+#include "CompositorResource.h"
+#include "compositor.h"
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+namespace Compositor{
+
+//
+
+}
+
---
+++ src/CompositorFont.h
@@ -0,0 +1,11 @@
+#ifndef COMPOSITOR_FONT_H
+#define COMPOSITOR_FONT_H
+
+namespace Compositor{
+
+//
+
+}
+
+#endif
+
