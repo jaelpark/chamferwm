dd5a240afed376ad1bd10d9c71626feeb8feadd6
jparkkil <jasper.parkkila@cern.ch>
2019-11-16 14:54:19 +0200

    Use minImageCount

--- src/compositor.cpp
+++ src/compositor.cpp
@@ -457,7 +457,8 @@ void CompositorInterface::InitializeRenderEngine(){
 	VkSwapchainCreateInfoKHR swapchainCreateInfo = {};
 	swapchainCreateInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
 	swapchainCreateInfo.surface = surface;
-	swapchainCreateInfo.minImageCount = 3;
+	//swapchainCreateInfo.minImageCount = 3;
+	swapchainCreateInfo.minImageCount = surfaceCapabilities.minImageCount;
 	swapchainCreateInfo.imageFormat = VK_FORMAT_B8G8R8A8_UNORM;
 	swapchainCreateInfo.imageColorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR;
 	swapchainCreateInfo.imageExtent = imageExtent;
